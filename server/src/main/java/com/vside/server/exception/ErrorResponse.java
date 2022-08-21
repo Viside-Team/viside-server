c80b603a2bea48d0827d3d8c360bb24afd6c52d0
dldbdud314 <dldbdud314@naver.com>
2022-08-21 16:27:21 +0900

    fix: 예외 처리

--- server/src/main/java/com/vside/server/exception/ErrorResponse.java
+++ server/src/main/java/com/vside/server/exception/ErrorResponse.java
@@ -1,8 +1,10 @@
 package com.vside.server.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public class ErrorResponse{
     private final String errorMessage;
 }
