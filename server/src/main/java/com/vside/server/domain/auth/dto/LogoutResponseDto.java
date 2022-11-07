d01a929740c9c1a3ad3ba1cc90266315dbd7f75a
JJONSOO <pjs970616@naver.com>
2022-11-07 19:27:54 +0900

    feat : create logoutResponseDto

---
+++ server/src/main/java/com/vside/server/domain/auth/dto/LogoutResponseDto.java
@@ -0,0 +1,14 @@
+package com.vside.server.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LogoutResponseDto {
+    private String message;
+}
