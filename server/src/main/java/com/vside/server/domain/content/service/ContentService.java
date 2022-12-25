1f630bff83acb3e212d5ce008a9c7e96bfea5041
dldbdud314 <dldbdud314@naver.com>
2022-12-25 20:21:35 +0900

    chore: import 문 정리

--- server/src/main/java/com/vside/server/domain/content/service/ContentService.java
+++ server/src/main/java/com/vside/server/domain/content/service/ContentService.java
@@ -15,7 +15,6 @@ import com.vside.server.domain.keyword.dao.KeywordRepository;
 import com.vside.server.domain.scrap.dao.ScrapRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
