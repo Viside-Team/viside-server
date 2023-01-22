d4582236b6a225cb73b5a370e238a972563a00a9
dldbdud314 <dldbdud314@naver.com>
2023-01-22 20:34:02 +0900

    feat: 24개까지 표출하는 코드 Pagination 적용+최근  날짜순 정렬하기

--- server/src/main/java/com/vside/server/domain/scrap/dao/ScrapRepository.java
+++ server/src/main/java/com/vside/server/domain/scrap/dao/ScrapRepository.java
@@ -4,6 +4,7 @@ import com.vside.server.domain.content.Entity.Content;
 import com.vside.server.domain.scrap.Entity.Scrap;
 import com.vside.server.domain.user.Entity.User;
 import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -15,8 +16,8 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
     int countByUserUserId(Long userId);
 
-    @Query(value = "select c from Scrap s, Content c where s.content=c and s.user.userId=:userId")
-    List<Content> findScrapContentsByUserId(@Param("userId") Long userId);
+    @Query(value = "select c from Scrap s, Content c where s.content=c and s.user.userId=:userId order by s.scrapDate desc")
+    List<Content> findScrapContentsByUserId(@Param("userId") Long userId, Pageable pageable);
 
     Scrap findByContentAndUser(Content content, User user);
 
